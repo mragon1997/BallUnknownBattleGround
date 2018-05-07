d6807dd2d40c95e5d6a3ac9613f0c4d473f9d9ee
mragon <916102026@qq.com>
2018-05-07 17:00:35 +0800

    base-layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+<div class="game">
+  <div class="game-content">
+
+    <div class="main-content">
+      <Ball></Ball>
+    </div>
+    <div class="sidebar">
+      <div class="game-map">
+
+      </div>
+    </div>
+
   </div>
+
+
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import Ball from './components/ball.vue';
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Ball
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  font-size: 16px;
+}
+
+.game {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #ededed;
+}
+
+.game-content {
+  width: 850px;
+  height: 600px;
+  margin: 20px auto;
+  background: #ccc;
+}
+
+.main-content {
+  width: 600px;
+  height: 600px;
+  float: left;
+}
+
+.sidebar {
+  width: 250px;
+  height: 600px;
+  float: left;
+  box-sizing: border-box;
+  background: #ede387;
+}
+
+.game-map {
+  width: 250px;
+  height: 250px;
+  box-sizing: border-box;
+  border: solid 1px black;
 }
 </style>
---
+++ src/components/ball.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+ <div>
+  <img src="../assets/logo.png" alt="">
+
+ </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="css">
+img{
+ width: 50px;
+}
+</style>
